c0fd5d10e05d7ddf17f8080a6870b6ac652f0c9b
notnsas <ramapanjinararendra@gmail.com>
2026-04-14 23:47:40 +0700

    uibuild

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
-const cors = require('cors')
 
-app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     { 
