9c399d974c73a3ad629d16ab4e6bdcdd5ca0f2ec
notnsas <ramapanjinararendra@gmail.com>
2026-04-15 04:36:33 +0700

    Added few for db

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
+const Person = require('./models/person')
 
 app.use(express.static('dist'))
-
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 // const requestLogger = (request, response, next) => {
 //   console.log('Method:', request.method)
